04536ec72f4c3ea375c64a6b4bb9140c671c2c96
jospnunes <jospnunes@gmail.com>
2022-12-02 20:56:37 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+              child: Column(
+        children: [
+          Image.asset(
+            'images/logo-nurse.png',
+            height: 150,
+          ),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Email',
+            ),
+          ),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Senha',
+            ),
+          ),
+          TextButton(
+            style: TextButton.styleFrom(
+              backgroundColor: Colors.red,
+            ),
+            onPressed: null,
+            child: Text("Entrar"),
+          )
+        ],
+      ))),
+    );
+  }
+}
